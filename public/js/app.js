024875fcee5cdd772a449fd8ca7c7b3a423bd3aa
Erik Cunningham <djtrinary@gmail.com>
2015-07-01 00:42:03 -0700

    a circle of friends

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+
+/*************************************
+//
+// global-socket app
+//
+**************************************/
+/* global io: false */
+/* global window: false */
+/* global document: false */
+/* global requestAnimationFrame: false */
+/* jshint -W015 */
+/* jshint camelcase: false */
+
+(function () {
+  'use strict';
+  // connect to our socket server
+  var socket = io.connect('http://127.0.0.1:7008/');
+
+  var width = window.innerWidth,
+      height = window.innerHeight;
+
+  var svg = d3.select("body")
+    .append("svg")
+    .attr({
+      width: width,
+      height: height
+    });
+  var users = d3.map();
+  var container = svg.append("g").classed("container", true);
+  container.attr("transform", "translate(400,400)");
+
+  function addTweet(tweet) {
+    users.set(tweet.id, tweet);
+    console.log(users.entries());
+    container.selectAll(".sharer").data(users.entries(), function(d) { return d.key; })
+      .attr({
+        cx: function (d, i) { return Math.sin(2 * Math.PI / users.size() * i) * 200 ; },
+        cy: function (d, i) { return Math.cos(2 * Math.PI / users.size() * i) * 200 ; },
+        r: 10
+      })
+      .enter()
+      .append("circle")
+      .classed("sharer", true);
+  }
+
+  socket.on('tweet', addTweet);
+}());
---
+++ server.js
@@ -0,0 +1,53 @@
+/*************************************
+//
+// global-socket app
+//
+**************************************/
+(function() {
+  'use strict';
+
+  // express magic
+  var express = require('express');
+  var app = express();
+  var server = require('http').createServer(app);
+  var io = require('socket.io').listen(server);
+  var device  = require('express-device');
+  var ps = require('powersocket');
+
+  var runningPortNumber = process.env.PORT;
+
+
+  app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
+
+    //set the view engine
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+
+    app.use(device.capture());
+  });
+
+
+  // logs every request
+  app.use(function (req, res, next) {
+    console.log({method: req.method, url: req.url, device: req.device});
+    // goes onto the next function in line
+    next();
+  });
+
+  app.get('/', function (req, res) {
+    res.render('index', {});
+  });
+
+  ps.configuration({
+    username: process.env.POWERSOCKET_USER,
+    password: process.env.POWERSOCKET_PASS,
+    path:     process.env.POWERSOCKET_PATH
+  });
+
+  ps.callback(function (d) {
+    io.sockets.emit('tweet', d);
+  });
+
+  server.listen(runningPortNumber);
+}());
